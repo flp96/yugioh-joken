43b3fdfb4de4b460c1c6b8d7366e8ec6d9ddc7ad
flp96 <filipefernandes__@live.com>
2024-12-12 00:24:13 -0300

    favicon e cursor personalizado adicionados

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -74,19 +74,39 @@ async function createCardImage(IdCard, fieldSide) {
 
 async function setCardsField(cardId) {
   await removeAllCardsImages();
+  await hiddenCardDetails();
+  await showHiddenCardFieldsImages(true);
 
   let computerCardId = await getRandomCardId();
 
-  state.fieldCards.player.style.display = "block";
-  state.fieldCards.computer.style.display = "block";
-
-  state.fieldCards.player.src = cardData[cardId].img;
-  state.fieldCards.computer.src = cardData[computerCardId].img;
+  await drawCardsInField(cardId, computerCardId);
 
   let duelResults = await checkDuelResults(cardId, computerCardId);
   
   await updateScore();
   await drawButton(duelResults);
+}
+
+async function drawCardsInField(cardId, computerCardId) {
+  state.fieldCards.player.src = cardData[cardId].img;
+  state.fieldCards.computer.src = cardData[computerCardId].img;
+}
+
+async function showHiddenCardFieldsImages(value) {
+  if (value === true) {
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+  } 
+  if (value === false) {
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+  }
+}
+
+async function hiddenCardDetails() {
+  state.cardSprites.avatar.src = "";
+  state.cardSprites.name.innerText = "";
+  state.cardSprites.type.innerText = "";
 }
 
 async function drawButton(text) {
@@ -161,8 +181,11 @@ async function playAudio(status) {
 
 
 function init(){
-    drawCards(5, state.playerSides.player1);
-    drawCards(5, state.playerSides.computer);
+ 
+  showHiddenCardFieldsImages(false);
+
+  drawCards(5, state.playerSides.player1);
+  drawCards(5, state.playerSides.computer);
 }
 
 
